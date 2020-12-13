81827b0ccafa087819ba0b98ae9063e1015beae3
bzls <2395913652@qq.com>
2020-12-13 13:43:23 +0800

    now

--- MainInterface/demo.php
+++ MainInterface/demo.php
@@ -1,11 +1,10 @@
 <?php
-echo"gitceshi";
 $nameId=$_POST["nameId"];
 $number=$_POST["number"];
 $password=$_POST["password"];
 echo "账号是：",$number;
 echo "密码是：",$password;
-@$conn=new mysqli("localhost:3306","root","","demo");
+@$conn=new mysqli("localhost:3306","root",".Wyj2458941097","demo");
 if($conn->connect_error){
     die("连接失败".$conn->connect_error);
 }
